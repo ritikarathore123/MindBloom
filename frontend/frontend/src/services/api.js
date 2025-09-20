77e81223fe20aa50f2a268532629501b082864d4
ritikarathre123 <ritikarathore211@gmail.com>
2025-09-20 16:18:34 +0530

    origin

--- frontend/frontend/src/services/api.js
+++ frontend/frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://mindbloom-backend-00pi.onrender.com/api',
   withCredentials: true,
 
   timeout: 10000, 
